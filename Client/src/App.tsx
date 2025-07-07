b816d319145cdbfa6698bcca54cf53230f33762e
AmrKhaled35 <amrk6965@gmail.com>
2025-07-07 20:59:36 +0300

    Fix : Reload Problem

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
+
 import LandingPage from './pages/LandingPage';
 import WorkshopsPage from './pages/WorkshopsPage';
 import WorkshopDetailsPage from './pages/WorkshopDetailsPage';
 import LoadingScreen from './components/LoadingScreen';
+function RedirectOnReload() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname !== '/') {
+      navigate('/');
+    }
+  }, []);
+
+  return null;
+}
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -12,6 +31,7 @@ function App() {
     <>
       {loading && <LoadingScreen />}
       <Router>
+        <RedirectOnReload />
         <Routes>
           <Route path="/" element={<LandingPage setLoading={setLoading} />} />
           <Route path="/workshops" element={<WorkshopsPage setLoading={setLoading} />} />
